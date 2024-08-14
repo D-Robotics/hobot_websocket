acbb225b790efe0f39c420d7c402077e0e7d9a60
ying.zeng <ying.zeng@d-robotics.cc>
2024-08-14 07:17:52 +0000

    : Adjusted the color of the background category

--- webservice/html/TogetheROS/main.js
+++ webservice/html/TogetheROS/main.js
@@ -9,7 +9,7 @@ let socketParameters = getURLParameter();
 let pathNum = getVideoNum();
 let pathNumArr  = [...new Array(pathNum).keys()];
 let bgr_putpalette = [
-  128, 64 , 128, 244, 35 , 232, 70 , 70 , 70 , 102, 102, 156, 190, 153, 153, 
+  0  , 0  , 0  , 244, 35 , 232, 70 , 70 , 70 , 102, 102, 156, 190, 153, 153, 
   153, 153, 153, 250, 170, 30 , 220, 220, 0  , 107, 142, 35 , 152, 251, 152, 
   0  , 130, 180, 220, 20 , 60 , 255, 0  , 0  , 0  , 0  , 142, 0  , 0  , 70 , 
   0  , 60 , 100, 0  , 80 , 100, 0  , 0  , 230, 119, 11 , 32 , 216, 191, 69 , 
@@ -413,9 +413,9 @@ function transformData(buffer) {
               let floatdata = []
               item['floatMatrixs_'][0]['arrays_'].map(values => {
                 values['value_'].map(index => {
-                  floatdata.push(bgr_putpalette[(index % 81) * 3], 
-                                 bgr_putpalette[(index % 81) * 3 + 1], 
-                                 bgr_putpalette[(index % 81) * 3 + 2], 
+                  floatdata.push(bgr_putpalette[(index >= 80? (index % 80) + 1: index) * 3], 
+                                 bgr_putpalette[(index >= 80? (index % 80) + 1: index) * 3 + 1], 
+                                 bgr_putpalette[(index >= 80? (index % 80) + 1: index) * 3 + 2], 
                                  155)
                 })
               })
